89d6178b9535069c664e9e66dccdad4ddb087fbd
salman <salmann9977@gmail.com>
2023-07-23 05:08:35 +0530

    adding heading and cards in  dashboard

--- components/ui/mobile-sidebar.tsx
+++ components/ui/mobile-sidebar.tsx
@@ -4,8 +4,19 @@ import { Button } from "./button";
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./sheet";
 import Sidebar from "./sidebar";
+import { useEffect, useState} from "react";
 
 const MobileSidebar = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    },[]);
+
+    if(!isMounted) {
+        return null;
+    }
+    
     return (
         <Sheet>
             <SheetTrigger>
