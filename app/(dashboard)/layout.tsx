c0152497768dc51aa232fc46aa4ba47df27025e8
salman <salmann9977@gmail.com>
2023-07-24 00:42:04 +0530

    adding api limit action and free-counter ui card in the sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,15 +1,19 @@
 import Navbar from "@/components/ui/navbar";
 import Sidebar from "@/components/ui/sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
 
-const DashboardLayout = ({
+const DashboardLayout = async ({
     children
 }: {
     children: React.ReactNode;
 }) => {
+    const apiLimitCount = await getApiLimitCount();
+    // const isPro = await checkSubscription();
+
     return (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-                <Sidebar />
+                <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
             </div>
             <main className="md:pl-72">
                 <Navbar />
